a8140fdbb870ea6cbd4dd22f5f2b92fc768e5b02
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-21 16:24:34 +0700

    Именованные маршруты. Ошибка на rspec исправлена 2.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit home_path
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit home_path
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit home_path
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
