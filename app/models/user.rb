7740b8fe0baa8344a0a0ecadbe0b86b9f617a144
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-25 11:31:15 +0700

    Тесты authenticate...

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ActiveRecord::Base
 	# виртуальные атрибуты (не сохраняются в БД)
 	attr_accessor :password, :password_confirmation
 
-
 	before_save { self.email = email.downcase }
 
-
-	validates :name, {
-		presence: true,
-		length: { maximum: 50 },
+	validates :name, { 
+		presence: true, 
+		length: { maximum: 50 }
 	}
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
 	validates :email, {
 		presence: true,
 		format: { with: VALID_EMAIL_REGEX },
