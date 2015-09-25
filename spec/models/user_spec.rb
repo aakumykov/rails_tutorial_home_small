b0b6207d1b42f8910f03b514ddf7682e9d6ba774
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-25 16:32:16 +0700

    Проверка на пустоту имени тестируется.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,15 +12,15 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
-  #it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_confirmation) }
   #it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-  # describe "when name is not present" do
-  #   before { @user.name = " " }
-  #   it { should_not be_valid }
-  # end
+  describe 'когда отсутствует имя' do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
 
   # describe "when email is not present" do
   #   before { @user.email = " " }
