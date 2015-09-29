9fe295e12313f27409a2fc09e762ac8458327546
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-29 16:40:09 +0700

    Действие users#create c функцией user_params()

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,43 @@
 class UsersController < ApplicationController
+
+  # new
   def new
   	@user = User.new
   end
 
+  # index
   def index
   end
 
+  # show
   def show
   	@user = User.find(params[:id])
   end
+
+  # create
+  def create
+  	@user = User.new(user_params)    # Not the final implementation!
+
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
