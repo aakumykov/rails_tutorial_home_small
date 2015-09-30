d4b63c7db82718db33a58962687e1a5c1c7f6fba
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-30 10:34:32 +0700

    Обработка входа с неверными данными.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 	
-	# страница входа (создания новой сессии
+	# страница входа (создания новой сессии)
 	def new
 	end
 
 	# создание сессии
 	def create
+	  user = User.find_by(email: params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+	    # Sign the user in and redirect to the user's show page.
+	  else
+	    flash.now[:error] = 'Неверная комбинация логин/пароль'
+	    render 'new'
+	  end
 	end
 
 	# удаление сессии
