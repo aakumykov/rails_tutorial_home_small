ca719e12b343fc22243fe0b8ffd064e4d7b6c394
Андрей Кумыков <aakumykov@yandex.ru>
2015-09-30 16:14:07 +0700

    ... и миграция с amember_token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,9 @@ class SessionsController < ApplicationController
 	def create
 	  user = User.find_by(email: params[:session][:email].downcase)
 	  if user && user.authenticate(params[:session][:password])
-	    # Sign the user in and redirect to the user's show page.
+	    sign_in user
+	    redirect_to user
+	    #redirect_to user_path(user.id)
 	  else
 	    flash.now[:error] = 'Неверная комбинация логин/пароль'
 	    render 'new'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,9 +8,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def home
-  end
-
   def signup
   end
 end
