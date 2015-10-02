cf1316db19cf10405d563665d57f6ad1619ea8b1
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-03 00:06:56 +0700

    Косметичиские улучшения в spec/requests/user_pages_spec.rb в ходе его осознания.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "User pages" do
   describe 'страница профиля,' do
     let(:user) { FactoryGirl.create(:user) }
     
-    before { visit user_path(user) }
+    before(:each) { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
@@ -16,7 +16,7 @@ describe "User pages" do
 
   describe 'страница регистрации,' do
 
-    before { visit signup_path }
+    before(:each) { visit signup_path }
 
     it { should have_title("Sign up") }
     it { should have_content('Страница регистрации') }
@@ -24,25 +24,25 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe ', неверно заполненная,' do
-      it "should not create a user" do
+      it 'не должен создаваться пользователь,' do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
 
     describe 'верно заполненная,' do
-      before do
+      before(:each) do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
 
-      #it ', должен быть создан пользователь' do
-      #  expect { click_button submit }.to change(User, :count).by(1)
-      #end
+      it 'должен быть создан пользователь,' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
 
       describe 'после регистрации,' do
-        before { click_button submit }
+        before(:each) { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
         it { should have_link('Sign out') }
