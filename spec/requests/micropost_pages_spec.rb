30c554972a4afc4925b2b585da61cf539b31c4f5
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-13 17:10:49 +0700

    Частичные шаблоны на главной странице и куча ошибок на тестах.

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,11 +1,35 @@
 require 'spec_helper'
 
-describe "MicropostPages" do
-  describe "GET /micropost_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get micropost_pages_index_path
-      response.status.should be(200)
-    end
-  end
+describe 'Страницы микросообщений,' do
+
+	subject { page }
+
+	let(:user) { FactoryGirl.create(:user) }
+
+	before(:each) { sign_in user }
+
+	describe 'создание микросообщения,' do
+		before(:each) { visit root_path }
+
+		describe 'с неверными данными,' do
+			
+			it 'микросообщение не должно создаваться,' do
+				expect { click_button 'Отправить' }.not_to change(Micropost, :count)
+			end
+
+			describe 'должно появиться сообщение об ошибке,' do
+				before { click_button 'Отправить' }
+				it { should have_content('error') }
+			end
+		end
+
+		describe 'с верными данными,' do
+			before { fill_in 'micropost_content', with: "Lorem ipsum" }
+			
+			it 'должно создаваться микросообщение,' do
+				expect { click_button 'Отправить' }.to change(Micropost, :count).by(1)
+			end
+		end
+	end
+
 end
