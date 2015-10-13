05fa9853ed30a57935897e3d883111cd9bd954c2
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-13 15:29:45 +0700

    Перенёс помошник signed_in_user в sessions_helper.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,13 +72,6 @@ class UsersController < ApplicationController
       )
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: 'Пожалуйста, войдите.'
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url, alert: 'Нельзя редактировать чужой профиль.' unless current_user?(@user)
