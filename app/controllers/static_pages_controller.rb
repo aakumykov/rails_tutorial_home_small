6af5fcf3407224129ae425c449fa1e8415a2224e
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-19 09:58:31 +0700

    8 микросообщений на главной странице.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
 	if signed_in?
   		@user = current_user
   		@micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 8)
   	end
   end
 
