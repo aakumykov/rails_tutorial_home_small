3259cc7fb85b90b44d8df1578d57582fc982f178
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-21 09:31:21 +0700

    Связь пользователь-взаимоотношение.

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-describe Relationship do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Взаимоотношения,' do
+
+  let(:reader) { FactoryGirl.create(:user) }
+  let(:author) { FactoryGirl.create(:user) }
+  let(:relationship) { reader.relationships.build(author_id: author.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe 'методы читателя,' do
+    it { should respond_to(:reader) }
+    it { should respond_to(:author) }
+    its(:reader) { should eq reader }
+    its(:author) { should eq author }
+  end
 end
