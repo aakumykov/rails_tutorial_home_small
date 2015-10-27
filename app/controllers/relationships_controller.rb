266d7df098c4d411e0f45f38210a4720f87ab261
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-27 14:02:27 +0700

    Контроллер Relationship с действиями.

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,17 @@
+class RelationshipsController < ApplicationController
+	before_action :signed_in_user
+
+	def create
+	    @author = User.find(params[:relationship][:author_id])
+	    current_user.read!(@author)
+	    redirect_to @author
+	end
+
+	def destroy
+	    @author = Relationship.find(params[:id]).author
+	    # вариант
+	    # @author = current_user.relationships.find_by(id: params[:id]).author
+	    current_user.unread!(@author)
+	    redirect_to @author
+	end
+end
