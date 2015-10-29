f9f789618943de1d12c08e3d977012f829ecd8a5
Андрей Кумыков <aakumykov@yandex.ru>
2015-10-29 11:36:57 +0700

    Fix: follow/unfollow через AJAX.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,21 +2,21 @@ class RelationshipsController < ApplicationController
 	before_action :signed_in_user
 
 	def create
-		@author = User.find(params[:relationship][:author_id])
-		current_user.read!(@author)
+		@user = User.find(params[:relationship][:author_id])
+		current_user.read!(@user)
 		respond_to do |format|
-			format.html { redirect_to @author }
+			format.html { redirect_to @user }
 			format.js
 		end
 	end
 
 	def destroy
-		@author = Relationship.find(params[:id]).author
+		@user = Relationship.find(params[:id]).author
 		# вариант
-		# @author = current_user.relationships.find_by(id: params[:id]).author
-		current_user.unread!(@author)
+		# @user = current_user.relationships.find_by(id: params[:id]).author
+		current_user.unread!(@user)
 		respond_to do |format|
-			format.html { redirect_to @author }
+			format.html { redirect_to @user }
 			format.js
 		end
 	end
